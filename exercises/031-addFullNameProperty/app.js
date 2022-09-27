11b2ccbd720d987609a9647106043a94708e9aa2
lusferror <luisalfredorobles09@gmail.com>
2022-09-27 20:56:12 -0300

    llegue hasta el ejercio 32

--- exercises/031-addFullNameProperty/app.js
+++ exercises/031-addFullNameProperty/app.js
@@ -1,4 +1,13 @@
 function addFullNameProperty(obj) {
   // Add your code after this line
-  
+  var fullName={
+    firstName:obj.firstName,
+    lastName:obj.lastName,
+  };
+  fullName.fullName=`${obj.firstName} ${obj.lastName}`;
+  return fullName;
+}
+const cosa={
+  firstName:"Jaden",
+  lastName:"Smith"
 }
