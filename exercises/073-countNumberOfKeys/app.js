79b92113150de83777d5b738761ef39bbb8d44d9
lusferror <luisalfredorobles09@gmail.com>
2022-09-28 21:20:48 +0000

    hasta el ejercicio 74

--- exercises/073-countNumberOfKeys/app.js
+++ exercises/073-countNumberOfKeys/app.js
@@ -1,4 +1,11 @@
 function countNumberOfKeys(obj) {
     // your code here
-
+    return Object.keys(obj).length;
 }
+let obj = {
+    a: 5,
+    b: 4,
+    c: 2,
+    d: 1
+};
+console.log(countNumberOfKeys(obj))
