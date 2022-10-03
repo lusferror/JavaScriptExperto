5158ea5fe2e702cf374b77fb4e7d17d481492386
lusferror <luisalfredorobles09@gmail.com>
2022-10-03 15:19:09 -0300

    al fin termine

--- exercises/157-phoneNumber/app.js
+++ exercises/157-phoneNumber/app.js
@@ -5,19 +5,23 @@ function PhoneNumberFormatter(numbers) {
 PhoneNumberFormatter.prototype.render = function() {
   let string = '';
   // your code here
+  string=this.parenthesize(this.getAreaCode())+" "+this.getExchangeCode()+"-"+this.getLineNumber();
   return string;
 };
 
 PhoneNumberFormatter.prototype.getAreaCode = function() {
   // your code here
+  return this.slice(0,3);
 };
 
 PhoneNumberFormatter.prototype.getExchangeCode = function() {
   // your code here
+  return this.slice(3,6)
 };
 
 PhoneNumberFormatter.prototype.getLineNumber = function() {
   // your code here
+  return this.slice(6)
 };
 
 PhoneNumberFormatter.prototype.parenthesize = function(string) {
@@ -27,3 +31,6 @@ PhoneNumberFormatter.prototype.parenthesize = function(string) {
 PhoneNumberFormatter.prototype.slice = function(start, end) {
   return this.numbers.slice(start, end).join('');
 };
+let arr=[6, 5, 0, 8, 3, 5, 9, 1, 7, 2]
+let output = new PhoneNumberFormatter(arr);
+console.log(output.render())
--- exercises/158-longestPalindrome/app.js
+++ exercises/158-longestPalindrome/app.js
@@ -3,13 +3,78 @@ function findLongestPalindrome(sentence) {
   // iterate words and collect the palindromes
   // sort the list of palindromes by word length
   // return the largest item in the sorted list
+  let palindromo = isPalindrome(sentence.toLowerCase());
+  let mayor = ""
+  if (palindromo.length < 2) {
+    mayor = palindromo.join("")
+  }
+  else {
+    for (let i = 0; i < palindromo.length - 1; i++) {
+      let a = palindromo[i];
+      let b = palindromo[i + 1];
+      if (sortAscendingByLength(a, b) == 1 || sortAscendingByLength(a, b) == 0) {
+        mayor = palindromo[i];
+      }
+      else if (sortAscendingByLength(a, b) == -1) {
+        mayor = palindromo[i + 1];
+      }
+    }
+  }
+  sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~'()]/gi, "").split(" ").forEach(element=>{
+    if(mayor==element.toLowerCase()){
+      mayor=element;
+    }
+  })
+  return mayor;
 }
 
 function reverseString(string) {
+  let frase = Array.from(string).reverse().join("").split(" ");
+  return frase;
 }
 
 function isPalindrome(word) {
   // hint: you can detect palindromes by comparing a string to its reverse
+  let palabra = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~'()]/gi, "");
+  let texto = palabra.split(" ");
+  let frase = reverseString(palabra);
+  let palindromo = [];
+  let aux = [];
+  texto.forEach((element, indice) => {
+    frase.forEach((item, index) => {
+      if (element == item) {
+        palindromo.push(element);
+        console.log()
+        if ((indice != 0 && index != frase.length - 1) || !(indice != texto.length - 1 && index != 0)) {
+          if (texto[indice - 1][texto[indice - 1].length - 1] + element + texto[indice + 1][0] == frase[index - 1][frase[index - 1].length - 1] + item + frase[index + 1][0]) {
+            palindromo.push(texto[indice - 1][texto[indice - 1].length - 1] + " " + element + " " + texto[indice + 1][0])
+          }
+          else if (element + texto[indice + 1][0] == item + frase[index + 1][0]) {
+            palindromo.push(element + " " + texto[indice + 1][0]);
+          }
+          else if (texto[indice - 1][texto[indice - 1].length - 1] + element == frase[index - 1][frase[index - 1].length - 1] + item) {
+            palindromo.push(texto[indice - 1][texto[indice - 1].length - 1] + element);
+          }
+
+        }
+        else if (indice == 0 && index == 0) {
+          if (element + texto[indice + 1][0] == item + frase[index + 1][0]) {
+            palindromo.push(element + " " + texto[indice + 1][0])
+          }
+        }
+        else if (indice == texto.length - 1 && index == frase.length - 1) {
+          if (texto[indice - 1][texto[indice - 1].length - 1] + element == frase[index - 1][frase[index - 1].length - 1] + item) {
+            palindromo.push(texto[indice - 1][texto[indice - 1].length - 1] + element);
+          }
+        }
+      }
+    });
+  });
+  let arr = new Set(palindromo);
+  for (let i of arr) {
+    aux.push(i);
+  }
+  return aux;
 }
 
 function sortAscendingByLength(a, b) {
@@ -20,3 +85,8 @@ function sortAscendingByLength(a, b) {
   }
   return 0;
 }
+// let output = "Madam, i mada";
+// // console.log(reverseString(output.toLowerCase()))
+// console.log(isPalindrome(output.toLowerCase()))
+// console.log(findLongestPalindrome("My dad is a racecar athlete"));
+console.log(findLongestPalindrome("Madam, i'm adam"));
